cb7e94d41408aa3ecf9d19cd7f4687135e4908a5
naina <nanu02d8@gmail.com>
2020-11-11 17:29:00 +0530

    Add Google spreadsheet api for posting users' signup details to google sheets

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ button.addEventListener("click", () => {
     console.log("hey")
     alert("Please Signup/Login first");
 })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzieAYOt7nrn5UAlJZsyEu-VoDtYmWQz1d9PSCQABmq8kT1T6cj/exec'
+const form = document.forms['signup']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks for Signing Up, you may now login to our website.."))
+    .catch(error => console.error('Error!', error.message))
+});
