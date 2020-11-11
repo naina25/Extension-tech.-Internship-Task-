99ee14c1f8687f3c1635e276ff00331009cabf47
naina <nanu02d8@gmail.com>
2020-11-11 21:10:47 +0530

    Start running local server and render html page via local server

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-let button = document.getElementById("button");
+// let button = document.getElementById("button");
+// button.addEventListener("click", () => {
+//     console.log("hey")
+//     alert("Please Signup/Login first");
+// })
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbzieAYOt7nrn5UAlJZsyEu-VoDtYmWQz1d9PSCQABmq8kT1T6cj/exec'
+// const form = document.forms['signup']
 
-button.addEventListener("click", () => {
-    console.log("hey")
-    alert("Please Signup/Login first");
-})
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzieAYOt7nrn5UAlJZsyEu-VoDtYmWQz1d9PSCQABmq8kT1T6cj/exec'
-const form = document.forms['signup']
+// form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => alert("Thanks for Signing Up, you may now login to our website.."))
+//     .catch(error => console.error('Error!', error.message))
+// });
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+const express = require('express');
+const bodyParser = require('body-parser');
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thanks for Signing Up, you may now login to our website.."))
-    .catch(error => console.error('Error!', error.message))
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + "/index.html")
 });
+
+app.post('/', (req,res) => {
+
+})
+
+app.listen('3000',() => {
+    console.log("Server has started on port 3000");
+})
